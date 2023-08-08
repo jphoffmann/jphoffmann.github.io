f04036322b328d8d87882699ad847db8a531b17a
jphoffmann <e1505330@student.tuwien.ac.at>
2023-08-08 16:26:17 +0200

    Added Flowbite Svelte plugin. Fixed bug where Tooltip would show up when hovering over its hidden state. Added Gradle and Maven icons.

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -1,32 +1,35 @@
-<script lang="ts" src="./TW-ELEMENTS-PATH/dist/js/tw-elements.umd.min.js">
+<script lang="ts">
+    import {Tooltip} from 'flowbite-svelte';
     import projects from '$lib/projects.json';
 
     let tech = new Map<string, string>(Object.entries(projects.tech));
 
     export let items: string[];
+
+    /*
+    <!--Tooltip-->
+    <span class="absolute left-1/2 -translate-x-1/2 -translate-y-12
+    opacity-0 peer-hover:opacity-100 transition-opacity
+    px-3 py-2.5 w-max rounded-lg
+    bg-dark-grey text-grey font-semibold text-center"
+    >
+        {item}
+        <!--Tooltip arrow-->
+        <span class="absolute top-full left-1/2 -ml-[8px] border-[8px] border-t-dark-grey border-transparent"/>
+    </span>
+     */
 </script>
 
 <div class="flex flex-wrap gap-2.5">
     {#each items as item}
         {#if tech.has(item)}
-            <div class="group inline-block relative rounded-[0.5rem] p-1
-            hover:bg-[hsl(0,0%,90%,1)]
-            transition duration-300 ease-in-out">
-
-                <!--Tooltip-->
-                <span class="absolute left-1/2 -translate-x-1/2 -translate-y-12
-                opacity-0 group-hover:opacity-100 transition-opacity
-                px-3 py-2.5 w-max rounded-lg
-                bg-dark-grey text-grey font-semibold text-center">
-                    {item}
-                    <!--Tooltip arrow-->
-                    <span class="absolute top-full left-1/2 -ml-[8px] border-[8px] border-t-dark-grey border-transparent"/>
-                </span>
-
-                <!--Logo-->
+            <div class="inline-block relative rounded-[0.5rem] p-1
+            hover:bg-[hsl(0,0%,90%,1)] transition duration-300 ease-in-out">
                 <img src="/icons/{tech.get(item)}" alt="{item} Logo"
                      class="min-w-[40px] max-w-[100px] h-[30px] laptop:h-[40px]"/>
             </div>
+
+            <Tooltip type="custom" class="bg-dark-grey text-grey font-semibold" >{item}</Tooltip>
         {/if}
     {/each}
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,32 +1,35 @@
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/tw-elements/dist/js/**/*.js"],
+    content: ["./src/**/*.{html,js,svelte,ts}",
+        "./node_modules/tw-elements/dist/js/**/*.js",
+        "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    screens: {
-      'tablet': '640px',
-      'laptop': '1280px',
-      'desktop': '2560px'
+    theme: {
+        screens: {
+            'tablet': '640px',
+            'laptop': '1280px',
+            'desktop': '2560px'
+        },
+        extend: {
+            colors: {
+                'white': '#F3F3F3',
+                'grey': '#F4F3EE',
+                'grey-alt': '#ebede9',
+                'grey-highlight': '#d0cfcb',
+                'medium-grey': '#606060',
+                'dark-grey': '#383646',
+                'blue': '#C9DBD7',
+                'medium-blue': '#89b7ae',
+                'dark-blue': '#10141f',
+                'black': '#181717',
+                // flowbite-svelte
+                primary: { 50: '#FFF5F2', 100: '#FFF1EE', 200: '#FFE4DE', 300: '#FFD5CC', 400: '#FFBCAD', 500: '#FE795D', 600: '#EF562F', 700: '#EB4F27', 800: '#CC4522', 900: '#A5371B'}
+            },
+        },
     },
-    extend: {
-      colors: {
-        'white': '#F3F3F3',
-        'grey': '#F4F3EE',
-        'grey-alt': '#ebede9',
-        'grey-highlight': '#d0cfcb',
-        'medium-grey': '#606060',
-        'dark-grey': '#383646',
-        'blue':'#C9DBD7',
-        'medium-blue':'#89b7ae',
-        'dark-blue':'#10141f',
-        'black': '#181717',
-      },
-    },
-  },
 
-  plugins: [require("tw-elements/dist/plugin")],
-  darkMode: "class"
+    plugins: [require('flowbite/plugin')],
+    darkMode: "class"
 };
 
 module.exports = config;
