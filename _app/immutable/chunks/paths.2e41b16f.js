6124875b235fc0e8e7281f5d4dd57073a14827ad
Jakob Hoffmann <e1505330@student.tuwien.ac.at>
2023-08-10 10:41:42 +0200

    Updates

--- _app/immutable/chunks/paths.2e41b16f.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_b8n14q)==null?void 0:s.base)??"/jphoffmann.github.io";var a;const e=((a=globalThis.__sveltekit_b8n14q)==null?void 0:a.assets)??t;export{e as a,t as b};
---
+++ _app/immutable/chunks/paths.b7fdca4d.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_123jtu8)==null?void 0:s.base)??"/jphoffmann.github.io";var t;const e=((t=globalThis.__sveltekit_123jtu8)==null?void 0:t.assets)??a;export{e as a,a as b};
